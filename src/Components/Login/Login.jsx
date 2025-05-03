fd51a77a6cc8c17dcbfe770ee233d0533b4030d6
soponalways <soponalways@gmail.com>
2025-05-03 07:31:09 +0600

    create login and registration form using daisy ui ,

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault(); 
+        const email = e.target.email.value; 
+        const password = e.target.password.value; 
+        console.log(email, password)
+    }
     return (
-        <div>
-            Hello this is login 
+        <div className="card bg-base-100 mx-auto mt-10 w-full max-w-sm shrink-0 shadow-2xl">
+            <h3 className="text-3xl text-center font-bold">Login now!</h3>
+            <div className="card-body">
+                <form onSubmit={handleLogin} className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" name='email' className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" name='password' className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </form>
+                <p>New to this website? Please <Link to={'/SignUp'} className='text-blue-400 underline'>Sign Up</Link></p>
+            </div>
         </div>
     );
 };
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const SignUp = () => {
+    const handleSignUp = e => {
+        e.preventDefault() ; 
+        const name = e.target.name.value; 
+        const email = e.target.email.value ; 
+        const password = e.target.password.value; 
+        console.log(name, email, password)
+    }
     return (
-        <div>
-            Hello this is Sign UP Page 
+        <div className="card bg-base-100 mx-auto mt-10 w-full max-w-sm shrink-0 shadow-2xl">
+            <h3 className="text-3xl text-center font-bold">Sign Up now!</h3>
+            <div className="card-body">
+                <form onSubmit={handleSignUp} className="fieldset">
+                    <label className="label">Name</label>
+                    <input type="text" name='name' className="input" placeholder="Name" />
+                    <label className="label">Email</label>
+                    <input type="email" name='email' className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" name='password' className="input" placeholder="Password" />
+                    <button className="btn btn-neutral mt-4">Sign Up</button>
+                </form>
+                <p>Already Have an account? Please <Link to={'/Login'} className='text-blue-400 underline'>Login</Link></p>
+            </div>
         </div>
     );
 };
